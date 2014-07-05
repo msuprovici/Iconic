8927f19ec8b48ad20ea5c52b289c697c80d18efb
Mike Suprovici <msadmin@Mike-Suprovicis-MacBook-Pro.local>
2014-07-05 10:43:04 -0700

    added pftableviewcontroller to account view controller.  my teams showed in the user profile.

--- AccountViewController.h
+++ AccountViewController.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 #import <Parse/Parse.h>
 
-@interface AccountViewController : UITableViewController
+@interface AccountViewController : PFQueryTableViewController
 
 @property (strong, nonatomic) IBOutlet PFImageView *myProfilePhoto;
 
--- AccountViewController.m
+++ AccountViewController.m
@@ -16,11 +16,29 @@
 
 @implementation AccountViewController
 
-- (id)initWithStyle:(UITableViewStyle)style
-{
-    self = [super initWithStyle:style];
+- (id)initWithCoder:(NSCoder *)aCoder {
+    self = [super initWithCoder:aCoder];
     if (self) {
-        // Custom initialization
+        
+        // Custom the table
+        
+        // The className to query on
+        self.parseClassName = kTeamTeamsClass;
+        
+        // The key of the PFObject to display in the label of the default cell style
+        self.textKey = kTeams;
+        
+        // Uncomment the following line to specify the key of a PFFile on the PFObject to display in the imageView of the default cell style
+        // self.imageKey = @"image";
+        
+        // Whether the built-in pull-to-refresh is enabled
+        self.pullToRefreshEnabled = YES;
+        
+        // Whether the built-in pagination is enabled
+        self.paginationEnabled = YES;
+        
+        // The number of objects to show per page
+        self.objectsPerPage = 25;
     }
     return self;
 }
@@ -68,31 +86,167 @@
     // Dispose of any resources that can be recreated.
 }
 
-#pragma mark - Table view data source
+#pragma mark - PFQueryTableViewController
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-//#warning Potentially incomplete method implementation.
-    // Return the number of sections.
-    return 0;
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-//#warning Incomplete method implementation.
-    // Return the number of rows in the section.
-    return 0;
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    static NSString *CellIdentifier = @"Cell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+- (void)objectsWillLoad {
+    [super objectsWillLoad];
     
-    // Configure the cell...
-    
-    return cell;
+    // This method is called before a PFQuery is fired to get more objects
 }
+
+- (void)objectsDidLoad:(NSError *)error {
+    [super objectsDidLoad:error];
+    
+    // This method is called every time objects are loaded from Parse via the PFQuery
+}
+
+
+ // Override to customize what kind of query to perform on the class. The default is to query for
+ // all objects ordered by createdAt descending.
+ - (PFQuery *)queryForTable {
+     
+ //*this value needs to be replaced with a dynamic value*
+ PFUser * user = [PFUser currentUser];//<- use dynamic value here
+     
+ PFQuery *query = [PFQuery queryWithClassName:self.parseClassName];
+ 
+ PFQuery *teamPlayersClass = [PFQuery queryWithClassName:kTeamPlayersClass];
+ [teamPlayersClass whereKey:kUserObjectIdString equalTo:user.objectId];
+     [query whereKey:@"objectId" matchesKey:kTeamObjectIdString inQuery:teamPlayersClass];
+ 
+ // If Pull To Refresh is enabled, query against the network by default.
+ if (self.pullToRefreshEnabled) {
+ query.cachePolicy = kPFCachePolicyNetworkOnly;
+ }
+ 
+ // If no objects are loaded in memory, we look to the cache first to fill the table
+ // and then subsequently do a query against the network.
+ if (self.objects.count == 0) {
+ query.cachePolicy = kPFCachePolicyCacheThenNetwork;
+ }
+ 
+ [query orderByDescending:@"createdAt"];
+ 
+ return query;
+ }
+
+
+/*
+ // Override to customize the look of a cell representing an object. The default is to display
+ // a UITableViewCellStyleDefault style cell with the label being the textKey in the object,
+ // and the imageView being the imageKey in the object.
+ - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath object:(PFObject *)object {
+ static NSString *CellIdentifier = @"Cell";
+ 
+ PFTableViewCell *cell = (PFTableViewCell *)[tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+ if (cell == nil) {
+ cell = [[PFTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+ }
+ 
+ // Configure the cell
+ cell.textLabel.text = [object objectForKey:self.textKey];
+ cell.imageView.file = [object objectForKey:self.imageKey];
+ 
+ return cell;
+ }
+ */
+
+/*
+ // Override if you need to change the ordering of objects in the table.
+ - (PFObject *)objectAtIndex:(NSIndexPath *)indexPath {
+ return [self.objects objectAtIndex:indexPath.row];
+ }
+ */
+
+/*
+ // Override to customize the look of the cell that allows the user to load the next page of objects.
+ // The default implementation is a UITableViewCellStyleDefault cell with simple labels.
+ - (UITableViewCell *)tableView:(UITableView *)tableView cellForNextPageAtIndexPath:(NSIndexPath *)indexPath {
+ static NSString *CellIdentifier = @"NextPage";
+ 
+ UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+ 
+ if (cell == nil) {
+ cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+ }
+ 
+ cell.selectionStyle = UITableViewCellSelectionStyleNone;
+ cell.textLabel.text = @"Load more...";
+ 
+ return cell;
+ }
+ */
+
+#pragma mark - UITableViewDataSource
+
+/*
+ // Override to support conditional editing of the table view.
+ - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+ // Return NO if you do not want the specified item to be editable.
+ return YES;
+ }
+ */
+
+/*
+ // Override to support editing the table view.
+ - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+ if (editingStyle == UITableViewCellEditingStyleDelete) {
+ // Delete the object from Parse and reload the table view
+ } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+ // Create a new instance of the appropriate class, and save it to Parse
+ }
+ }
+ */
+
+/*
+ // Override to support rearranging the table view.
+ - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+ }
+ */
+
+/*
+ // Override to support conditional rearranging of the table view.
+ - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+ // Return NO if you do not want the item to be re-orderable.
+ return YES;
+ }
+ */
+
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [super tableView:tableView didSelectRowAtIndexPath:indexPath];
+}
+
+
+
+
+//#pragma mark - Table view data source
+//
+//- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+//{
+////#warning Potentially incomplete method implementation.
+//    // Return the number of sections.
+//    return 0;
+//}
+//
+//- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+//{
+////#warning Incomplete method implementation.
+//    // Return the number of rows in the section.
+//    return 0;
+//}
+//
+//- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+//{
+//    static NSString *CellIdentifier = @"Cell";
+//    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+//    
+//    // Configure the cell...
+//    
+//    return cell;
+//}
+
 
 /*
 // Override to support conditional editing of the table view.
