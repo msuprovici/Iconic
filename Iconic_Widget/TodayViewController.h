c0f78e17a9fdd474595650f8abfa725e79d1a188
Mike Suprovici <msadmin@Mike-Suprovicis-MacBook-Pro.local>
2014-10-03 10:43:57 -0700

    Initial set-up for a widget.

---
+++ Iconic_Widget/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  Iconic_Widget
+//
+//  Created by Mike Suprovici on 10/3/14.
+//  Copyright (c) 2014 Explorence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
