bfc33414934d71ab96cfd078072670c44b8602ea
Mike Suprovici <msadmin@Mike-Suprovicis-MacBook-Pro.local>
2015-05-08 16:19:35 -0700

    layer integration

--- Iconic/AppDelegate.m
+++ Iconic/AppDelegate.m
@@ -26,6 +26,8 @@
 #import "ParseCrashReporting/ParseCrashReporting.h"
 #import <FacebookSDK/FacebookSDK.h>
 #import "AchievmentsViewController.h"
+#import <LayerKit/LayerKit.h>
+
 
 
 @interface AppDelegate ()
@@ -70,6 +72,25 @@
     //initialize Parse
     [Parse setApplicationId:@"LiLukyOROzCPxkNIvG9SD6nPMFdZsFNxzYsB06LT"
                   clientKey:@"NWnu7sQiFf9t3vyruSWQ8CqepFjKQh7IAZr8b3WA"];
+    
+    
+    
+    
+    //layer
+    // Initializes a LYRClient object
+    NSUUID *appID = [[NSUUID alloc] initWithUUIDString:@"42b66e50-f517-11e4-9829-c8f500001922"];
+    LYRClient *layerClient = [LYRClient clientWithAppID:appID];
+    
+    // Tells LYRClient to establish a connection with the Layer service
+    [layerClient connectWithCompletion:^(BOOL success, NSError *error) {
+        if (success) {
+            NSLog(@"Layer Client is Connected!");
+        }
+    }];
+    
+    
+    
+    
     
        
     //initialize Amplitude analytics
